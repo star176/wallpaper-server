4a938c8799dc333e086de739d5e5152177d918da
GaoShaoshan <gao_shaoshan@cdv.com>
2024-03-22 14:21:41 +0800

    返回图片地址

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { readdir } from 'fs/promises';
+import { resolve } from 'path';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async get() {
+    const files = await readdir(resolve(__dirname, '../wallpaper'));
+    const url =
+      'http://localhost:3000/wallpaper/' +
+      files[Math.floor(Math.random() * files.length - 1)];
+    return url;
   }
 }
